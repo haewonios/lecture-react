941ab91b9b25caf3245b836af7ac9185c72642a8
haewonios <haewon.dev@gmail.com>
2024-04-26 01:48:26 +0900

    검색어 입력시 리셋버튼 노출

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,15 +1,32 @@
-import { qs } from "../helpers.js";
+import { qs, on } from "../helpers.js";
 import View from "./View.js";
+
+const tag = "[SearchFormView]";
 
 export default class SearchFormView extends View {
   constructor() {
+    console.log(tag, "constructor");
+
     super(qs("#search-form-view"));
 
+    this.inputElement = qs("[type=text]", this.element);
     this.resetElement = qs("[type=reset]", this.element);
+
     this.showResetButton(false);
+    this.bindEvent();
   }
 
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvent() {
+    on(this.inputElement, "keyup", () => this.handleKeyUp());
+  }
+
+  handleKeyUp() {
+    console.log(tag, "handleKeyUp", this.inputElement.value);
+    const { value } = this.inputElement; // destructuring 으로 value 만 사용
+    this.showResetButton(value.length > 0);
   }
 }
