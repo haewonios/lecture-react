712206add862c008220118be81ae285212d8da9f
haewonios <haewon.dev@gmail.com>
2024-04-26 02:01:29 +0900

    submit 시 검색결과 컨트롤러에 위임

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -13,20 +13,28 @@ export default class SearchFormView extends View {
     this.resetElement = qs("[type=reset]", this.element);
 
     this.showResetButton(false);
-    this.bindEvent();
+    this.bindEvents();
   }
 
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
   }
 
-  bindEvent() {
+  bindEvents() {
     on(this.inputElement, "keyup", () => this.handleKeyUp());
+    on(this.element, "submit", (event) => this.handleSubmit(event));
   }
 
   handleKeyUp() {
     console.log(tag, "handleKeyUp", this.inputElement.value);
     const { value } = this.inputElement; // destructuring 으로 value 만 사용
     this.showResetButton(value.length > 0);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault(); // enter 입력시 page reload 방지
+    console.log(tag, "handleSubmit");
+    const { value } = this.inputElement;
+    this.emit("@submit", { value });
   }
 }
