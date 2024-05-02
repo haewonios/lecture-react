8a948c2c10d9706585ddb06050321679897842a8
haewonios <haewon.dev@gmail.com>
2024-05-02 21:46:14 +0900

    리액트로 구현

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,9 +1,183 @@
+import { formatRelativeDate } from "./js/helpers.js";
+import store from "./js/Store.js";
+
+const TabType = {
+  KEYWORD: "KEYWORD",
+  HISTORY: "HISTORY",
+};
+
+const TabLabel = {
+  [TabType.KEYWORD]: "추천 검색어",
+  [TabType.HISTORY]: "최근 검색어",
+};
+
 // js 시작점
+// 전체 화면 담당
+class App extends React.Component {
+  constructor() {
+    super();
 
-const element = (
-  <header>
-    <h2 className="container">검색</h2>
-  </header>
-);
+    this.state = {
+      searchKeyword: "",
+      searchResult: [],
+      submitted: false,
+      selectedTab: TabType.KEYWORD,
+      keywordList: [],
+      historyList: [],
+    };
+  }
 
-ReactDOM.render(element, document.querySelector("#app")); // 가상 DOM 생성
+  componentDidMount() {
+    const keywordList = store.getKeywordList();
+    const historyList = store.getHistoryList();
+
+    this.setState({ keywordList, historyList });
+  }
+
+  handleChangeInput(event) {
+    const searchKeyword = event.target.value;
+
+    if (searchKeyword.length <= 0 && this.state.submitted) {
+      return this.handleReset();
+    }
+
+    this.setState({ searchKeyword }); // 비동기
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.search(this.state.searchKeyword);
+  }
+
+  search(searchKeyword) {
+    const searchResult = store.search(searchKeyword);
+    const historyList = store.getHistoryList();
+
+    this.setState({
+      searchKeyword,
+      searchResult,
+      historyList,
+      submitted: true,
+    }); // 변경된 필드만 반영 - 다른 필드는 기존 값 유지
+    console.log("search", this.state.searchKeyword);
+  }
+
+  handleReset() {
+    this.setState({
+      searchKeyword: "",
+      searchResult: [],
+      submitted: false,
+    });
+  }
+
+  handleClickRemoveHistory(event, keyword) {
+    event.stopPropagation(); // 이벤트 버블링 방지
+    store.removeHistory(keyword);
+    const historyList = store.getHistoryList();
+    this.setState({ historyList });
+  }
+
+  render() {
+    const searchForm = (
+      <form
+        onSubmit={(event) => this.handleSubmit(event)}
+        onReset={() => this.handleReset()}
+      >
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요."
+          autoFocus
+          value={this.state.searchKeyword}
+          onChange={(event) => this.handleChangeInput(event)}
+        />
+        {this.state.searchKeyword.length > 0 && (
+          <button type="reset" className="btn-reset"></button>
+        )}
+      </form>
+    );
+
+    const searchResult =
+      this.state.searchResult.length > 0 ? (
+        <ul className="result">
+          {this.state.searchResult.map((item) => {
+            return (
+              <li key={item.id}>
+                <img src={item.imageUrl} alt={item.name} />
+                <p>{item.name}</p>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div className="empty-box">검색 결과가 없습니다.</div>
+      );
+
+    const keywordList = (
+      <ul className="list">
+        {this.state.keywordList.map(({ id, keyword }, index) => {
+          return (
+            <li key={id} onClick={() => this.search(keyword)}>
+              <span className="number">{index + 1}</span>
+              <span>{keyword}</span>
+            </li>
+          );
+        })}
+      </ul>
+    );
+
+    const historyList = (
+      <ul className="list">
+        {this.state.historyList.map(({ id, keyword, date }) => {
+          return (
+            <li key={id} onClick={() => this.search(keyword)}>
+              <span>{keyword}</span>
+              <span className="date">{formatRelativeDate(date)}</span>
+              <button
+                className="btn-remove"
+                onClick={(event) =>
+                  this.handleClickRemoveHistory(event, keyword)
+                }
+              ></button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+
+    const tabs = (
+      <>
+        <ul className="tabs">
+          {Object.values(TabType).map((tabType) => {
+            return (
+              <li
+                className={this.state.selectedTab === tabType ? "active" : ""}
+                key={tabType}
+                onClick={() => this.setState({ selectedTab: tabType })}
+              >
+                {TabLabel[tabType]}
+              </li>
+            );
+          })}
+        </ul>
+        {this.state.selectedTab === TabType.KEYWORD && keywordList}
+        {this.state.selectedTab === TabType.HISTORY && historyList}
+      </>
+    );
+
+    return (
+      <>
+        <header>
+          <h2 className="container">검색</h2>
+        </header>
+        <div className="container">
+          {searchForm}
+          <div className="content">
+            {this.state.submitted ? searchResult : tabs}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#app")); // 가상 DOM 생성
